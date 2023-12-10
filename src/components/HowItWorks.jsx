4d91994bc2c463e4bd323a1a90dde20425bb1d44
unknown <alahmed1rra@gmail.com>
2023-12-09 20:54:26 -0500

    hi

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -2,8 +2,18 @@ import '../styles/HowItWorks.css';
 
 function HowItWorks() {
   return (
-    <div>
-      <h2>How it Works Text Here</h2>
+    <div className="how-it-works-box" >
+      <h2>
+      Save your time and increase your comprehension
+      by pasting your text here in the text and our 
+      advance system will print you a list of nouns
+      and verbs. And to better enhance your learning, 
+      we added a feature to our system that allows you
+       to edit the lists adding verbs and nouns. You can
+        save your results as well! 
+
+      </h2>
+      
     </div>
   );
 }
