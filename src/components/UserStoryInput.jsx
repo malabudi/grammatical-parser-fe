1982c754d0981d7483d277b01d3ba526c49c74ae
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2023-12-09 20:47:02 -0500

    display api data

--- src/components/UserStoryInput.jsx
+++ src/components/UserStoryInput.jsx
@@ -1,4 +1,4 @@
-import '../styles/reportpage.css';
+import '../styles/UserStoryInput.css';
 import TextField from '@mui/material/TextField';
 
 function UserStoryInput(props) {
@@ -8,40 +8,42 @@ function UserStoryInput(props) {
 
   return (
     <div className='story-input'>
-      <TextField
-      required
-      id="standard-required"
-      label="User Story Title"
-      variant="filled"
-      sx={{
-        width: '600px',
-        marginBottom: '20px'
-      }}
-      />
-      <TextField
-      required
-      id="filled-multiline-static"
-      label="User Story Description"
-      multiline
-      rows={4}
-      variant="filled"
-      sx={{
-        width: '600px',
-        marginBottom: '20px'
-      }}
-      onChange={onChangeDesc}
-      />
-      <TextField
-      id="standard-required"
-      label="Story Subject"
-      variant="filled"
-      className='text-input'
-      helperText='Enter a noun that will be interacted with the most in the user story. This is to detect nouns more accurately'
-      sx={{
-        width: '600px',
-        marginBottom: '20px'
-      }}
-      />
+      <div>
+        <TextField
+        required
+        id="standard-required"
+        label="User Story Title"
+        variant="filled"
+        sx={{
+          width: '600px',
+          marginBottom: '20px'
+        }}
+        />
+        <TextField
+        required
+        id="filled-multiline-static"
+        label="User Story Description"
+        multiline
+        rows={4}
+        variant="filled"
+        sx={{
+          width: '600px',
+          marginBottom: '20px'
+        }}
+        onChange={onChangeDesc}
+        />
+        <TextField
+        id="standard-required"
+        label="Story Subject"
+        variant="filled"
+        className='text-input'
+        helperText='Enter a noun that will be interacted with the most in the user story. This is to detect nouns more accurately'
+        sx={{
+          width: '600px',
+          marginBottom: '20px'
+        }}
+        />
+      </div>
     </div>
   );
 }
--- src/pages/EditListsPage.jsx
+++ src/pages/EditListsPage.jsx
@@ -1,29 +1,41 @@
 import {useQuery} from 'react-query';
 
 function EditListsPage(props) {
-    /*As a database administrator, I want to back up our data daily so that we can recover information in case of data loss. */
+    /*As a database administrator, I want to back up our data daily so that we can recover information in case of data loss.*/
 
     const parseDesc = async () => {
 		const res = await fetch('http://localhost:5000/parse', {
             method: 'POST',
             mode: 'cors',
-            headers: {
-                'Content-Type': 'applications/json'
-            },
-            body: JSON.stringify(props.storyDesc)
+            headers: { 'Content-Type': 'application/json','Accept':'application/json' },
+            body: JSON.stringify({
+                story: props.storyDesc,
+                main_noun: ''
+            })
+        })
+        .catch((error) => {
+            console.error(error);
         });
+        
         const nounsVerbsObj = await res.json();
 		return nounsVerbsObj;
 	};
 
-    const {data, error, isLoading} = useQuery('nouns', parseDesc);
+    const { isLoading, error, data } = useQuery('nouns', parseDesc);
 
     if (error) return <div>Request Failed</div>;
 	if (isLoading) return <div>Loading...</div>;
 
     return (
         <div>
-            {data}
+            {data.nouns.map((item) => {
+                return (
+                <div>
+                    <h1>{item.associated_nouns}</h1>
+                    <h1>{item.associated_verbs}</h1>
+                </div>
+                );
+            })}
         </div>
     );
 }
