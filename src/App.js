572b2e966b9d253600d37c373e535aed60b3e444
sydneyeg <sydneyeg@umich.edu>
2023-12-10 19:27:22 -0500

    Verb and Noun list done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import HowItWorks from './components/HowItWorks';
 import UserStoryInput from './components/UserStoryInput';
 import ParseClear from './components/ParseClear';
 import Footer from './components/Footer';
-import ReportPage from './components/reportpage';
+import GrammarList from './components/GrammarList';
 
 function App() {
   return (
     <div className="App">
-      <ReportPage> </ReportPage>
+      <GrammarList />
     </div>
   );
 }
